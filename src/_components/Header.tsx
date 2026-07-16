39c3d8de8875af307a1e4e91f20f1bb833d41b4e
tyamaoka <tyamaoka@student.42tokyo.jp>
2026-07-16 16:51:24 +0900

    #51 Replace anchor tag with Link component for My Page navigation

--- src/_components/Header.tsx
+++ src/_components/Header.tsx
@@ -1,9 +1,11 @@
+import { Link } from "wasp/client/router"
+
 export function Header() {
 	return (
 		<header>
 			<img src='/favicon.svg' alt="EagerSleepers logo" />
 			<p>EagerSleepers</p>
-			<a href="/mypage">My Page</a>
+			<Link to="/mypage">My Page</Link>
 		</header>
 	)
 }
